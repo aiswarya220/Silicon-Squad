44ca9e4b873bffb1592b5c806817eab24cf6a336
aiswarya220 <aiswaryap2910@gmail.com>
2025-10-09 10:02:04 +0530

    PROJECT-5 DOC

---
+++ Project_5/vehicleHealthMonitoring/Core/Inc/ds18b20.h
@@ -0,0 +1,24 @@
+#ifndef DS18B20_H
+#define DS18B20_H
+
+#include "stm32f405xx.h"
+#include "system_stm32f4xx.h"
+#include <stdint.h>
+
+// ================= Configuration =================
+#define DS18B20_PORT GPIOC
+#define DS18B20_PIN  10   // <-- Change this based on your wiring
+
+// =============== Function Prototypes ===============
+void DS18B20_GPIO_Init(void);
+uint8_t DS18B20_Reset(void);
+void DS18B20_WriteByte(uint8_t data);
+uint8_t DS18B20_ReadByte(void);
+int16_t DS18B20_GetTemp(void);
+
+void DWT_Delay_Init(void);
+void delay_us(uint32_t us);
+void delay_ms(uint32_t ms);
+void DS18B20_StartConversion(void);
+
+#endif
---
+++ Project_5/vehicleHealthMonitoring/Debug/ThirdParty/freeRTOS/tasks.d
@@ -0,0 +1,25 @@
+ThirdParty/freeRTOS/tasks.o: ../ThirdParty/freeRTOS/tasks.c \
+ C:/Users/WZS1KOR/STM32CubeIDE/workspace_2/vehicleHealthMonitoring/ThirdParty/freeRTOS/include/FreeRTOS.h \
+ C:/Users/WZS1KOR/STM32CubeIDE/workspace_2/vehicleHealthMonitoring/ThirdParty/freeRTOS/FreeRTOSConfig.h \
+ C:/Users/WZS1KOR/STM32CubeIDE/workspace_2/vehicleHealthMonitoring/ThirdParty/freeRTOS/include/projdefs.h \
+ C:/Users/WZS1KOR/STM32CubeIDE/workspace_2/vehicleHealthMonitoring/ThirdParty/freeRTOS/include/portable.h \
+ C:/Users/WZS1KOR/STM32CubeIDE/workspace_2/vehicleHealthMonitoring/ThirdParty/freeRTOS/include/deprecated_definitions.h \
+ C:/Users/WZS1KOR/STM32CubeIDE/workspace_2/vehicleHealthMonitoring/ThirdParty/freeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/WZS1KOR/STM32CubeIDE/workspace_2/vehicleHealthMonitoring/ThirdParty/freeRTOS/include/mpu_wrappers.h \
+ C:/Users/WZS1KOR/STM32CubeIDE/workspace_2/vehicleHealthMonitoring/ThirdParty/freeRTOS/include/task.h \
+ C:/Users/WZS1KOR/STM32CubeIDE/workspace_2/vehicleHealthMonitoring/ThirdParty/freeRTOS/include/list.h \
+ C:/Users/WZS1KOR/STM32CubeIDE/workspace_2/vehicleHealthMonitoring/ThirdParty/freeRTOS/include/timers.h \
+ C:/Users/WZS1KOR/STM32CubeIDE/workspace_2/vehicleHealthMonitoring/ThirdParty/freeRTOS/include/task.h \
+ C:/Users/WZS1KOR/STM32CubeIDE/workspace_2/vehicleHealthMonitoring/ThirdParty/freeRTOS/include/stack_macros.h
+C:/Users/WZS1KOR/STM32CubeIDE/workspace_2/vehicleHealthMonitoring/ThirdParty/freeRTOS/include/FreeRTOS.h:
+C:/Users/WZS1KOR/STM32CubeIDE/workspace_2/vehicleHealthMonitoring/ThirdParty/freeRTOS/FreeRTOSConfig.h:
+C:/Users/WZS1KOR/STM32CubeIDE/workspace_2/vehicleHealthMonitoring/ThirdParty/freeRTOS/include/projdefs.h:
+C:/Users/WZS1KOR/STM32CubeIDE/workspace_2/vehicleHealthMonitoring/ThirdParty/freeRTOS/include/portable.h:
+C:/Users/WZS1KOR/STM32CubeIDE/workspace_2/vehicleHealthMonitoring/ThirdParty/freeRTOS/include/deprecated_definitions.h:
+C:/Users/WZS1KOR/STM32CubeIDE/workspace_2/vehicleHealthMonitoring/ThirdParty/freeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/WZS1KOR/STM32CubeIDE/workspace_2/vehicleHealthMonitoring/ThirdParty/freeRTOS/include/mpu_wrappers.h:
+C:/Users/WZS1KOR/STM32CubeIDE/workspace_2/vehicleHealthMonitoring/ThirdParty/freeRTOS/include/task.h:
+C:/Users/WZS1KOR/STM32CubeIDE/workspace_2/vehicleHealthMonitoring/ThirdParty/freeRTOS/include/list.h:
+C:/Users/WZS1KOR/STM32CubeIDE/workspace_2/vehicleHealthMonitoring/ThirdParty/freeRTOS/include/timers.h:
+C:/Users/WZS1KOR/STM32CubeIDE/workspace_2/vehicleHealthMonitoring/ThirdParty/freeRTOS/include/task.h:
+C:/Users/WZS1KOR/STM32CubeIDE/workspace_2/vehicleHealthMonitoring/ThirdParty/freeRTOS/include/stack_macros.h:
